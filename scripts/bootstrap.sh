16d19d5db7650c006d0654e83867a5458f3e8381
sprinzs123 <sprinzs351@gmail.com>
2024-12-12 09:50:18 -0500

    tryng to import helmrelease from common

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,4 +4,5 @@
 url="https://github.com/greenCircuit/flux-template-demo.git"
 branch="main"
 cluster="k3s"
-flux bootstrap git --url=${url} --path=clusters/${cluster} --branch=${branch}  --private-key-file=/home/${USER}/.ssh/flux-key
+
+flux bootstrap git --url=${url} --branch=main --path=./clusters/k3s/ --password=${GITHUB_TOKEN} 
