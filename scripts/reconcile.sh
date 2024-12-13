f671f0bdfe6b2aa5e31bb8e41e04e0d82991524f
sprinzs123 <sprinzs351@gmail.com>
2024-12-13 08:47:41 -0500

    name change

--- scripts/reconcile.sh
+++ scripts/reconcile.sh
@@ -2,3 +2,6 @@
 flux reconcile ks flux-system -n flux-system
 flux suspend ks flux-system -n flux-system
 flux resume ks flux-system -n flux-system
+
+# do helm releases reconcile
+flux reconcile hr cluster-definition -n flux-system
