ec7c133481f1b101ed8f695504a09ed40e9616fa
sprinzs123 <sprinzs351@gmail.com>
2024-12-13 11:45:28 -0500

    post install hook for deployment so can get correct replica set num, labels are creted correctly though

--- scripts/reconcile.sh
+++ scripts/reconcile.sh
@@ -8,6 +8,7 @@ flux reconcile hr cluster-definition -n flux-system
 flux suspend hr cluster-definition -n flux-system
 flux resume hr cluster-definition -n flux-system
 
-flux reconcile hr microservices -definition -n flux-system
+flux reconcile hr microservices -n flux-system
 flux suspend hr microservices -n flux-system
+sleep 2
 flux resume hr microservices -n flux-system
